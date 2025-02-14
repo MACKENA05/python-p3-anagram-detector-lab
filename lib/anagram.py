bcb3f50290e16ef6e1e4349cc916c6ba71f4dfc9
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-02-14 14:06:46 +0300

    anagram checker

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list_anagrams):
+        sorted_word = sorted(self.word)
+        return [word for word in list_anagrams if sorted(word) == sorted_word]
+
+
+
+anagram_checker = Anagram('listen')
+
+result = anagram_checker.match(['enlists', 'google', 'inlets', 'banana'])
+print(result)
